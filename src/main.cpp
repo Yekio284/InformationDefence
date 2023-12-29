ad7b0d75b0a885b7f201c9af6b9fc8c74599510d
Yekio284 <yan@DESKTOP-HVFJFTN>
2023-12-29 14:29:15 +0700

    Final commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,59 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <fstream>
 #include "crypto_library.hpp"
 
+typedef myCrypto::lab_fifth::Server lw5_Server;
+typedef myCrypto::lab_fifth::Client lw5_Client;
+
 int main() {
-    typedef myCrypto::RGR::Alice rgr_Alice;
-    typedef myCrypto::RGR::Bob rgr_Bob;
+    namespace lw1 = myCrypto::lab_first;
+    namespace lw3 = myCrypto::lab_third;
 
-    std::ifstream file("hamilton_files/hamilton_graph_info.txt");
-    std::ifstream file_cycle("hamilton_files/hamilton_cycle.txt");
+    int num_of_voters = 0;
+    std::cout << "Enter num of voters (or <= 0 to exit): ";
+    std::cin >> num_of_voters;
+
+    if (num_of_voters <= 0)
+        return 0;
+
+    std::remove("database.csv"); // Удаляем БД, если есть
+
+    lw5_Server server;
+
+    std::vector<lw5_Client> votersVec(num_of_voters);
+    for (auto &client : votersVec) {
+        client.setVote(lw1::random(0, 2)); // 0 - нет, 1 - да, 2 - воздержался
+        client.generate_n(server.getAddress()); 
+        client.generate_r(server.getN());
+        client.generate_h(server.getN());
+        client.compute_h1(server.getD(), server.getN());
+        
+        ll s1 = server.addVoterAndComputeS1(client.getId(), client.getH1());
+        if (s1 == -1) {
+            std::cout << "id: " << client.getId() << " Already in vote." << std::endl;
+            continue;
+        }
+
+        client.setS1(s1);
+        client.compute_s(server.getN());
+        
+        if (server.recieveBulletinAndAddToDB(client.getN(), client.getS(), client.getId()))
+            std::cout << "id<" << client.getId() << ">: good" << std::endl; 
+        else
+            std::cout << "id<" << client.getId() << ">: bad" << std::endl;  
+    }
+
+    // попытка поучаствовать еще раз для votersVec[7];
+    /*votersVec[7].setVote(lw1::random(0, 2)); // 0 - нет, 1 - да, 2 - воздержался
+    votersVec[7].generate_n(server.getAddress()); 
+    votersVec[7].generate_r(server.getN());
+    votersVec[7].generate_h(server.getN());
+    votersVec[7].compute_h1(server.getD(), server.getN());
     
-    // rgr_Graph graph(file, file_cycle);
-    // std::cout << graph.getN() << ' ' << graph.getM() << std::endl;
-    // graph.printHamiltonCycle();
-    // graph.printEdges();
-    // graph.printAdjacencyMatrix();
-    
-    rgr_Alice alice(file, file_cycle);
-   
-    //std::cout << std::boolalpha<< alice.match(alice.getAdjacencyMatrix(), alice.getAdjacencyMatrixH(), alice.getPermutation(), alice.Graph::getN());
-    //std::cout << std::noboolalpha << std::endl;
-   //
-    //std::cout << "Permutation: ";
-    //alice.printPermutation();
-//
-    //std::cout << "Hamilton cycle: ";
-    //alice.printHamiltonCycle();
-    //std::cout << std::endl;
-    //
-    //alice.printAdjacencyMatrix();
-    //std::cout << std::endl << std::endl;
-    //
-    //alice.printAdjacencyMatrixH();
-
-   // auto matrixF = alice.getAdjacencyMatrixF();
-
-    rgr_Bob bob(alice.getAdjacencyMatrixF());
+    ll s1 = server.addVoterAndComputeS1(votersVec[7].getId(), votersVec[7].getH1());
+    if (s1 == -1)
+        std::cout << "id<" << votersVec[7].getId() << "> Already in vote." << std::endl;*/
 
     return 0;
 }
